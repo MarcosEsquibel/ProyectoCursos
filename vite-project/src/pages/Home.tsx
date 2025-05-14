b95042a0be2418f43da5b6d5540421429a19d7e1
Jemina <jesquibelbernal@gmail.com>
2025-05-14 17:35:08 +0200

    Añadiendo imagen al home

--- vite-project/src/pages/Home.tsx
+++ vite-project/src/pages/Home.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { MdAssignment, MdLibraryBooks, MdSchool } from "react-icons/md";
 import { Link } from "react-router-dom";
 import "./Home.css";
+import psicologiapositiva2 from "../assets/images/psicologiapositiva2.png";
 
 const Home: React.FC = () => {
   function setDropdownOpen(arg0: boolean): void {
@@ -10,17 +11,21 @@ const Home: React.FC = () => {
 
   return (
     <div className="home-content">
-
       {/* Qué es NoimaLab - Sección inicial */}
       <section className="about-section">
         <h2>¿Qué es NoimaLab?</h2>
-        <p>
-          NoimaLab es un espacio online para quienes quieren transformar su vida desde dentro. Aquí te formas emocional, psicológica y vivencialmente, sin horarios ni tutorías. Tú marcas el ritmo, tú eliges el camino.
-        </p>
-        <img src="/ruta/ilustracion.png" alt="Ilustración de crecimiento personal" />
+        <div className="about-content">
+          <p>
+            NoimaLab es un espacio online para quienes quieren transformar su vida desde dentro. Aquí te formas emocional, psicológica y vivencialmente, sin horarios ni tutorías. Tú marcas el ritmo, tú eliges el camino.
+          </p>
+          <img
+            src={psicologiapositiva2}
+            alt="Ilustración de NoimaLab"
+            className="about-image"
+          />
+        </div>
       </section>
 
-   
       {/* Explora sección */}
       <section className="explore-section">
         <h2>Explora según tu necesidad</h2>
@@ -46,20 +51,19 @@ const Home: React.FC = () => {
         </div>
       </section>
 
-      
-
       {/* Frase final inspiradora */}
       <section className="closing-phrase">
-        <h3 style={{ textAlign: "center", marginTop: "3rem", color: "#333" }}>
+        <h3>
           'Entender nuestro interior no es opcional, es esencial'
         </h3>
       </section>
 
       {/* Footer */}
       <footer>
-        <p>Únete a NoimaLab y empieza tu propio camino </p>
-        <Link to="/register" className="hero-button">Empezar ahora</Link>
-         
+        <p>Únete a NoimaLab y empieza tu propio camino</p>
+        <Link to="/register" className="hero-button">
+          Empezar ahora
+        </Link>
       </footer>
     </div>
   );
@@ -67,4 +71,5 @@ const Home: React.FC = () => {
 
 export default Home;
 
-        
+
+          
